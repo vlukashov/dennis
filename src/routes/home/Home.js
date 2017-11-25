56896850218143824fa200764c7957cb7f57b7ab
Viktor Lukashov <viktor@vaadin.com>
2017-11-25 15:11:49 +0200

    add chat message

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 import './Home.css';
 
 import Header from '../../components/header/Header';
@@ -7,11 +8,15 @@ import ChatMessage from '../../components/chat-message/ChatMessage';
 class Home extends Component {
     render() {
       return (
-        <div>
+        <div className="Home">
           <Header title="Home" />
           <ChatMessage>
             Welcome Tim! It’s time to start your journey on making your teeth shine like you were a Hollywood star!
           </ChatMessage>
+          <div className="Home-chat-replies">
+            <Button>How do we do that?</Button>
+            <Button>Who are you?</Button>
+          </div>
         </div>
       );
     }
