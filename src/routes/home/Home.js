6042fe0c00d37aa0d69db4668bb598801850e844
Viktor Lukashov <viktor@vaadin.com>
2017-11-25 15:15:39 +0200

    formatting

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -6,20 +6,20 @@ import Header from '../../components/header/Header';
 import ChatMessage from '../../components/chat-message/ChatMessage';
 
 class Home extends Component {
-    render() {
-      return (
-        <div className="Home">
-          <Header title="Home" />
-          <ChatMessage>
-            Welcome Tim! It’s time to start your journey on making your teeth shine like you were a Hollywood star!
-          </ChatMessage>
-          <div className="Home-chat-replies">
-            <Button>How do we do that?</Button>
-            <Button>Who are you?</Button>
-          </div>
+  render() {
+    return (
+      <div className="Home">
+        <Header title="Home" />
+        <ChatMessage>
+          Welcome Tim! It’s time to start your journey on making your teeth shine like you were a Hollywood star!
+        </ChatMessage>
+        <div className="Home-chat-replies">
+          <Button>How do we do that?</Button>
+          <Button>Who are you?</Button>
         </div>
-      );
-    }
+      </div>
+    );
+  }
 }
   
 export default Home;
