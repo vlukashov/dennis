449d2f144d3a6cf4632cbc1b3a6a50bfe803bc20
Viktor Lukashov <viktor@vaadin.com>
2017-11-25 16:54:28 +0200

    add 'book an appointment' CTA

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
+import { Button, Panel, Image } from 'react-bootstrap';
 import './Home.css';
+import bookCtaImage from '../../assets/teeth-history.png';
 
 import Header from '../../components/header/Header';
 import ChatMessage from '../../components/chat-message/ChatMessage';
@@ -20,14 +21,29 @@ class Home extends Component {
     return (
       <div className="Home">
         <Header title="Home" />
-        <ChatMessage>
-          Welcome Tim! It’s time to start your journey on making your teeth shine like you were a Hollywood star!
-        </ChatMessage>
-        <div className="Home-chat-replies">
-          <Button>How do we do that?</Button>
-          <Button>Who are you?</Button>
+        <div className="Home-content">
+          <ChatMessage>
+            Welcome Tim! It’s time to start your journey on making your teeth shine like you were a Hollywood star!
+          </ChatMessage>
+          <div className="Home-chat-replies">
+            <Button>How do we do that?</Button>
+            <Button>Who are you?</Button>
+          </div>
+          <SubSection title="Your history" subtitle="See me history >" to="/my-history" />
+          <Panel>
+            <div className="Home-cta-panel">
+              <Image src={bookCtaImage}></Image>
+              <div className="Home-cta-panel-body">
+                <span>Schedule your first dental appointment to unlock the the history platform</span>
+                <h3>
+                  Book an appointment
+                  <Link to="/book-appointment">&gt;</Link>
+                </h3>
+              </div>
+            </div>
+            
+          </Panel>
         </div>
-        <SubSection title="Your history" subtitle="See me history >" to="/my-history" />
       </div>
     );
   }
