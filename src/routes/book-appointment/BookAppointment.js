c4e49c41da851626d4767547f5a17cbf44d0e34c
Viktor Lukashov <viktor@vaadin.com>
2017-11-26 12:46:48 +0200

    update the icons on the shecude a visit page

--- src/routes/book-appointment/BookAppointment.js
+++ src/routes/book-appointment/BookAppointment.js
@@ -5,7 +5,9 @@ import PageLayout from "../../layouts/page/PageLayout";
 import ChatMessage from "../../components/chat-message/ChatMessage";
 import InPageSubheader from "../../components/in-page-subheader/InPageSubheader";
 
-import actionImage from "../../assets/logo.svg";
+import checkupImage from "../../assets/3d-icon.svg";
+import regularImage from "../../assets/teeth-icon.svg";
+import urgentImage from "../../assets/exclamation-icon.svg";
 
 import "./BookAppointment.css";
 
@@ -18,7 +20,9 @@ class BookAppointment extends Component {
           style={{ background: props.background }}
         >
           <div className="ActionPanel-body">
-            <Image src={props.imgurl} />
+            <div className="ActionPanel-icon-wrapper">
+              <Image src={props.imgurl} />
+            </div>
             <div className="ActionPanel-content">{props.children}</div>
           </div>
         </Panel>
@@ -39,21 +43,21 @@ class BookAppointment extends Component {
 
         <ActionPanel
           background="linear-gradient(to bottom, #74ef9a 0%,#4eda6d 100%)"
-          imgurl={actionImage}
+          imgurl={checkupImage}
           className="BookAppointment-actionPanel"
         >
           I want a 3D scan checkup at the closest clinic.
         </ActionPanel>
         <ActionPanel
           background="linear-gradient(to bottom, #74D7EF 0%,#36C3F8 100%)"
-          imgurl={actionImage}
+          imgurl={regularImage}
           className="BookAppointment-actionPanel"
         >
           I want an full treatment with a professional dentist.
         </ActionPanel>
         <ActionPanel
           background="linear-gradient(to bottom, #F6B26C 0%,#FD9B37 100%)"
-          imgurl={actionImage}
+          imgurl={urgentImage}
           className="BookAppointment-actionPanel"
         >
           I have a tooth ache and need treatment ASAP.
